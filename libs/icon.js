2c256d928be5547b71776c207efa62598872ce90
Alican Özsırma <ozsirma@icloud.com>
2018-01-23 21:48:26 +0300

    Update icon.js
    update importing libraries

--- libs/icon.js
+++ libs/icon.js
@@ -1,4 +1,5 @@
-import React, { Component, Text } from 'react-native';
+import React, { Component } from 'react';
+import { Text } from 'react-native';
 
 export default class Icon extends Component {
   constructor(props, context) {
